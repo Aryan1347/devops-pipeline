3596af0e7a976d789d42b6c3b28c92993057993b
Aryan1347 <gawadearyan2@gmail.com>
2026-02-12 21:39:36 +0530

    feat: add ci pipeline

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("starting tests ... ");
+setTimeout(()=> console.log("waiting for 3 seconds"), 3000);
+setTimeout(() => console.log("waiting for 2 seconds"), 2000);
+setTimeout(() => console.log("waiting for 1 second"), 1000);
+console.log("tests completed ... ");
